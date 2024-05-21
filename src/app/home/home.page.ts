44d7779b95f2a2d7762b71b184ea7bafecd39d8a
Baanzito <diegocontatoprof@gmail.com>
2024-05-21 14:35:40 -0300

    Implantando Modulos e implementando o serviço da API Pokemon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit {
+  pokemon: any;
+
+  constructor(private pokemonService: PokemonService, private router: Router) {}
+
+  viewDetails() {
+    this.router.navigate(['/details', this.pokemon.id]);
+  }
+
+  ngOnInit(): void {
+    this.pokemonService.getPokemon(1).subscribe(data => {
+      this.pokemon = data;
+    }
+    );
+  }
 
 }
