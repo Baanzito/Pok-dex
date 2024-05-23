cb6ffd376e25a73f2bac66d3e7c6d5c9915ee3cc
Baanzito <diegocontatoprof@gmail.com>
2024-05-23 17:08:33 -0300

    Alterado Front-end da aplicação e incluido tratamento de erro no service da API

--- src/app/services/pokeapi.service.ts
+++ src/app/services/pokeapi.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,9 +18,25 @@ export class PokeapiService {
   getPokemonDetails(name: string): Observable<any> {
     return this.http.get<any>(`${this.baseUrl}/pokemon/${name}`).pipe(
       map((res: any) => {
+        // Convertendo a altura de decímetros para metros, pois a API retorna a altura em decímetros.
         res.height = res.height / 10;
         return res;
-      })
+      }),
+      // Tratamento de erros na requisição HTTP
+      catchError(this.handleError)
     );
+  }
+
+  // Manipula erros ocorridos durante requisições HTTP.
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Tratamento de erro do lado do cliente.
+      console.error('Ocorreu um erro:', error.error.message);
+    } else {
+      // Tratamento de erro do lado do servidor.
+      console.error(`Código do erro: ${error.status}, ` + `mensagem: ${error.error.message}`);
+    }
+    // Retornar um observable com uma mensagem de erro para o usuário.
+    return throwError('Ocorreu um erro ao processar sua solicitação. Por favor, tente novamente mais tarde.');
   }
 }
